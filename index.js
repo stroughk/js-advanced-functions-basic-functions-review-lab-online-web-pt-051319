f256424fa7ab9ec8b3f071afe247aca2b4cd40e2
Kateryna Strough <katerynastrough@hotmail.com>
2019-12-19 01:38:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ function saturdayFun(activity="roller-skate") {
 let mondayWork = function(activity="go to the office") {
   return `This Monday, I will ${activity}.`
 }
+
+let wrapAdjective = function(style="*") {
+  return function(adjective="special") {
+    return 'You are ${style}${adjective}${style}!'
+  }
+}
